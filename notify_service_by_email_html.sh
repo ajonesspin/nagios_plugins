36968c3ba95fbde7f42fcd7cd045049cc9d849e3
Kali Hernandez <kali.hernandez@sociomantic.com>
2014-08-08 13:35:40 +0200

    Add graphs for non-graphite notifications
    When the service check is not a graphite-based one, insert nagios' service
    history graph for more visual info.

--- notify_service_by_email_html.sh
+++ notify_service_by_email_html.sh
@@ -14,7 +14,8 @@ IMG_HEIGHT=200
 
 NAGIOS_BASEURL="https://YOUR_NAGIOS_BASEURL/nagios"
 GRAPHITE_BASEURL="https://YOUR_GRAPHITE_BASEURL/render/?width=${IMG_WIDTH}&height=${IMG_HEIGHT}&target="
-CURL_OPTIONS="-s -u YOUR_HTTP_USER:YOUR_HTTP_PASS"
+CURL_OPTIONS_GRAPHITE="-s -u GRAPHITE_HTTP_USER:GRAPHITE_HTTP_PASSWORD"
+CURL_OPTIONS_NAGIOS="-s -u YOUR_NAGIOS_HTTP_USER:YOUR_NAGIOS_HTTP_PASSWORD"
 
 declare -A COLORS_SERVICE
 COLORS_SERVICE[OK]='#88d066'
@@ -69,8 +70,9 @@ MESSAGE_BODY+="<table cellpadding=\"2px\" style=\"width: 100%; \">\n \
 "
 
 if [[ $NAGIOS_SERVICECHECKCOMMAND == check_graphite_metric* ]] ; then
+	# if alert comes from a graphite-monitored service, insert the graph and links
 	GRAPHITE_METRIC=`sed -r "s/^.*-m '(.*)'.*/\1/" <<< ${NAGIOS_SERVICECHECKCOMMAND} | sed "s/%HOST%/$NAGIOS_HOSTNAME/g"`
-	GRAPH_BASE64=$(/usr/bin/curl ${CURL_OPTIONS} "${GRAPHITE_BASEURL}${GRAPHITE_METRIC}&from=-6hours" | /usr/bin/base64 -w0)
+	GRAPH_BASE64=$(/usr/bin/curl ${CURL_OPTIONS_GRAPHITE} "${GRAPHITE_BASEURL}${GRAPHITE_METRIC}&from=-6hours" | /usr/bin/base64 -w0)
 	MESSAGE_BODY+="<div style=\"font-size: 10pt; font-family: monospace; text-align: center\">\n \
 		Have a look at the graph for the service in the last 6 hours:<br />\n \
 		<img src=\"data:image/png; base64,${GRAPH_BASE64}\" width=\"${IMG_WIDTH}\" height=\"${IMG_HEIGHT}\">\n \
@@ -81,11 +83,17 @@ if [[ $NAGIOS_SERVICECHECKCOMMAND == check_graphite_metric* ]] ; then
 		<a href='${GRAPHITE_BASEURL}${GRAPHITE_METRIC}&from=-1week'>Last week</a><br />\n \
 		<a href='${GRAPHITE_BASEURL}${GRAPHITE_METRIC}&from=-1month'>Last month</a><br />\n \
 		<a href='${GRAPHITE_BASEURL}${GRAPHITE_METRIC}&from=-1year'>Last year</a><br />\n \
-		</div>
-		<br />\n \
+		</div><br />\n \
+		"
+else
+	# else insert the service trend from nagios
+	GRAPH_BASE64=$(/usr/bin/curl ${CURL_OPTIONS_NAGIOS} "${NAGIOS_BASEURL}/cgi-bin/trends.cgi?createimage&host=${NAGIOS_HOSTNAME}&service=${NAGIOS_SERVICEDESC}" | /usr/bin/base64 -w0)
+	MESSAGE_BODY+="<div style=\"font-size: 10pt; font-family: monospace; text-align: center\">\n \
+		Have a look at the state history for the service:<br />\n \
+		<img src=\"data:image/png; base64,${GRAPH_BASE64}\">\n \
+		</div><br />\n \
 		"
 fi
-
 
 MESSAGE_BODY+="<div style=\"font-size: 9pt; font-family: monospace;\">\n \
 <ul>\n \
@@ -100,7 +108,5 @@ ${NAGIOS_LONGDATETIME}\n \
 "
 
 MESSAGE_SUBJECT="** ${NAGIOS_NOTIFICATIONTYPE} Service Alert: ${NAGIOS_HOSTALIAS}/${NAGIOS_SERVICEDESC} is ${NAGIOS_SERVICESTATE} **"
-
-#echo "emaileando a ${EMAIL_TARGET}" >> /tmp/nagios.log
 
 echo -e $MESSAGE_BODY | /usr/bin/mail.mailutils -a "Content-Type: text/html" -s "${MESSAGE_SUBJECT}" ${EMAIL_TARGET}
